8b192551e3b7a3dd28d6fc76d9af49be2c1acd01
Volnei Fuzer <volnei.fuzer@gmail.com>
2017-02-16 18:46:27 -0300

    inicio do projeto oficial

--- CreatorOfDynamicControls/CreatorOfDynamicControls.WebApplicationV2/CustomForm.ascx
+++ CreatorOfDynamicControls/CreatorOfDynamicControls.WebApplicationV2/CustomForm.ascx
@@ -1,4 +1,4 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="CustomForm.ascx.cs" 
     Inherits="CreatorOfDynamicControls.WebApplicationV2.CustomForm" %>
 
-<asp:Panel ID="Panel1" runat="server" value="false"></asp:Panel>
+<asp:Panel ID="Panel1" runat="server" valid="false"></asp:Panel>
--- CreatorOfDynamicControls/CreatorOfDynamicControls.WebApplicationV2/CustomForm.ascx.cs
+++ CreatorOfDynamicControls/CreatorOfDynamicControls.WebApplicationV2/CustomForm.ascx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Xml;
@@ -7,25 +8,32 @@ namespace CreatorOfDynamicControls.WebApplicationV2
 {
 	public partial class CustomForm : UserControl
 	{
-		public XmlDocument myXmlDoc { get; set; }
+		public string myXmlDoc { get; set; }
 
 		protected void Page_Load(object sender, EventArgs e)
 		{
 			if (IsPostBack)
 			{
 				Panel1.Controls.Clear();
-				toDrawControls();				
+				toDrawControls(myXmlDoc);
+			}
+			else
+			{
+				toDrawControls(myXmlDoc);
 			}
 		}
 
-		public static void toDrawControls()
+		public static void toDrawControls(string myXmlDoc)
 		{
-			throw new NotImplementedException();
+			List<WebControl> controls = new List<WebControl>();
+			List<string> preControl = new List<string>();
+			string controlType;
+
+			XmlTextReader xmlToRead = new XmlTextReader(myXmlDoc);
+
+
+
 		}
 
-		protected static void loadXML()
-		{
-			throw new NotImplementedException();
-		}
 	}
 }
